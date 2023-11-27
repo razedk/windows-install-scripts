6dd38b552722c3fbee5db5a62b8e621ab922dc14
Jacob Lundholm <jal@intelligentsystems.dk>
2023-11-27 18:19:13 -0500

    Updated with home alias

--- install-shell-addons/Microsoft.PowerShell_profile.ps1
+++ install-shell-addons/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,19 @@
 Set-Alias edit "C:\Program Files\Notepad++\notepad++.exe"
 Set-Alias showenv Show-Environment-Variables
 Set-Alias j Invoke-ZLocation
-Set-Alias home "cd $HOME"
+Set-Alias home Set-Location-Home
 
 # Method to show all environment variables
 function Show-Environment-Variables {
   param ($filter)
   Get-ChildItem -Path Env:\$filter*
 }
+
+# Method to cd to home folder
+function Set-Location-Home {
+  Set-Location "$HOME"
+}
+
 
 # Enable Starship
 #$ENV:STARSHIP_CONFIG = "$HOME\.starship\starship.toml"
